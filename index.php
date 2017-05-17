f0ba7e688947dd67758b34379c36d3a3a2a152cf
Bettina <bettina.ganter@gmx.de>
2017-05-17 20:38:36 +0200

    Start Files

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Pokedex - Kanto</title>
+<link rel= stylesheet href ="styles.css">
+</head>
+
+<body>
+	
+<?php ?>
+</body>
+
+
+</html>
---
+++ pokemon.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Pokedex - Kanto</title>
+<link rel= stylesheet href ="styles.css">
+</head>
+
+<body>
+
+<?php ?>
+</body>
+</html>
