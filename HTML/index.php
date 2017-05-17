28c09a41c27029b7475a49aac2cab6af67f03713
Bettina <bettina.ganter@gmx.de>
2017-05-17 21:25:37 +0200

    Absoluter Pfad zum Repository: C:\wamp64\www\Pokedex

---
+++ HTML/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<meta charset="UTF-8">
+<title>Pokedex - Kanto</title>
+<link rel= stylesheet href ="../CSS/styles.css">
+</head>
+
+<body>
+
+<div class="whiteTextfield">
+Hallo! Herzlich Willkommen in der Welt der Pokémon!<br>
+Hier erfährst du alles über die Pokemon in Kanto und 
+wo genau sie zufinden sind.<br>
+Viel Spaß!
+</div>	
+
+</body>
+
+
+</html>
